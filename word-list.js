fa300a6749cba2c8dbe0711ec6707c7249ffe91a
Breno1221 <123486496+Breno121zs@users.noreply.github.com>
2024-03-18 06:25:53 -0300

    Add files via upload

---
+++ word-list.js
@@ -0,0 +1,76 @@
+const wordList = [
+    {
+        word: "rifle",
+        hint: "Arma"
+    },
+    
+    {
+        word: "pistola",
+        hint: "Arma"
+    },
+
+    {
+        word: "sniper",
+        hint: "Arma"
+    },
+
+    {
+        word: "metralhadora",
+        hint: "Arma"
+    },
+
+    {
+        word: "faca",
+        hint: "Arma"
+    },
+
+    {
+        word: "granada",
+        hint: "Arma"
+    },      
+
+    {
+        word: "dust",
+        hint: "Mapa"
+    },
+
+    {
+        word: "mirage",
+        hint: "Mapa"
+    },
+
+    {
+        word: "inferno",
+        hint: "Mapa"
+    },
+
+    {
+        word: "headshot",
+        hint: "Termo de Jogo"
+    },
+
+    {
+        word: "bomba",
+        hint: "Equipamento"
+    },
+
+    {
+        word: "capacete",
+        hint: "Equipamento"
+    },
+
+    {
+        word: "desarmar",
+        hint: "Termo de Jogo"
+    },
+
+    {
+        word: "cache",
+        hint: "Mapa"
+    },
+
+    {
+        word: "breno",
+        hint: "Player"
+    },
+] 
